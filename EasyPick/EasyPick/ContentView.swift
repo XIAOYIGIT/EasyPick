34a9b7da78e90e9ea52f0a00b34d094f51081d6b
肖毅 <756514838@qq.com>
2019-12-13 11:44:21 +0800

    创建项目

---
+++ EasyPick/EasyPick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyPick
+//
+//  Created by 肖毅 on 2019/12/13.
+//  Copyright © 2019 肖毅. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
